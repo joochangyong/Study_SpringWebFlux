cebbd4ad993764b685ed758dda009866856e7d32
joochangyong <wnckddyd0525@gmail.com>
2021-11-08 11:53:18 +0900

    [ADD] 프로젝트 생성

---
+++ src/main/java/com/example/study_springwebflux/StudySpringWebFluxApplication.java
@@ -0,0 +1,13 @@
+package com.example.study_springwebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySpringWebFluxApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(StudySpringWebFluxApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/study_springwebflux/StudySpringWebFluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.study_springwebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySpringWebFluxApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
