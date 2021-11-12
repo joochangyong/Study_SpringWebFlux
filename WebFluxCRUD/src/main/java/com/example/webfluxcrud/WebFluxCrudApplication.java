ae0a5062f04f2cb7021bb120cea537e970d477bf
joochangyong <wnckddyd0525@gmail.com>
2021-11-12 17:46:06 +0900

    [ADD] WebFluxCRUD폴더 추가

---
+++ WebFluxCRUD/src/main/java/com/example/webfluxcrud/WebFluxCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.webfluxcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebFluxCrudApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WebFluxCrudApplication.class, args);
+  }
+
+}
---
+++ WebFluxCRUD/src/test/java/com/example/webfluxcrud/WebFluxCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webfluxcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebFluxCrudApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
